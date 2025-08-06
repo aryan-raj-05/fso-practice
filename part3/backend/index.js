115e50eb6455071b9b9f4d6a0d523014964fbbdf
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2025-08-06 17:40:50 +0530

    changes

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
 
 let notes = [
   {
@@ -29,7 +28,6 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(express.static('dist'))
-app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 
